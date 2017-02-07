65f6537d859b6e1c3b74af29213db859c5dcaa85
Walle Cyril <capocyril@hotmail.com>
2017-02-07 05:10:50 +0100

    changed starting experience

--- client/js/launcher.js
+++ client/js/launcher.js
@@ -45,7 +45,7 @@
     }
     ui.start();
 
-    const accepted = localData.get(MESSAGES.CONDITION_ACCEPTED);
+    const accepted = true; //localData.get(MESSAGES.CONDITION_ACCEPTED);
     
     const startServiceWorkerAndSockets = function () {
         serviceWorkerManager.start();
