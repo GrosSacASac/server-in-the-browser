25cf59fcd84f52fa9650a1746641a4c08f300a01
Walle Cyril <capocyril@hotmail.com>
2018-01-23 05:35:54 +0100

    indentation

--- client/js/ui.js
+++ client/js/ui.js
@@ -451,18 +451,18 @@ const ui = (function () {
             your_id: "not yet connected",
             localServerAvailability: false,
             userCode: `const http = require("http");
-    const hostname = "127.0.0.1";
-    const port = 3000;
+const hostname = "127.0.0.1";
+const port = 3000;
 
-    const server = http.createServer((req, res) => {
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/plain");
-      res.end("Hello World\\n");
-    });
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World\\n");
+});
 
-    server.listen(port, hostname, () => {
-      console.log(\`Server running at http://\${hostname}:\${port}/\`);
-    });
+server.listen(port, hostname, () => {
+  console.log(\`Server running at http://\${hostname}:\${port}/\`);
+});
     `
         });
 
