1ce440b5292749b6863350ddfabafbe8509d4e3c
Walle Cyril <capocyril@hotmail.com>
2018-04-25 23:09:13 +0200

    breaking changes

--- client/js/built/service_worker_with_version.js
+++ client/js/built/service_worker_with_version.js
@@ -33,7 +33,7 @@ https://heycam.github.io/webidl/#es-ByteString
 
 "use strict";
 
-const SERVICE_WORKER_VERSION = "0.20.38"; // updated with tools/service_worker_version.js (String)
+const SERVICE_WORKER_VERSION = "0.20.39"; // updated with tools/service_worker_version.js (String)
 const CACHE_VERSION = SERVICE_WORKER_VERSION;
 //const fileNamesToSaveInCache = ["/"];
 const HOME = "/";
