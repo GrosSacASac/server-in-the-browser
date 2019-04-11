55b53243d0183501ff18bee06bbfa23899e30fff
Walle Cyril <cyril.walle@protonmail.com>
2019-04-11 18:41:26 +0200

    Update messages.js

--- client/js/settings/messages.js
+++ client/js/settings/messages.js
@@ -1,21 +1,28 @@
 export {MESSAGES};
 
 const MESSAGES = {
-    EXIT: "exit",                           // the user leaves
-    LOADING_USER_LIST: "loading_user_list", // send the user list
+    EXIT: "exit",
+    LOADING_USER_LIST: "loading_user_list",
     SEND_ICE_CANDIDATE: "send_ice_candidate",
     RECEIVE_ICE_CANDIDATE: "receive_ice_candidate",
-    SEND_OFFER: "send_offer",
     SEND_DESCRIPTION: "send_description",
     RECEIVE_DESCRIPTION: "receive_description",
-    RECEIVE_OFFER: "receive_offer",
     LOCAL_SERVER_STATE: "LOCAL_SERVER_STATE",
     WELCOME: "welcome",
     SERVERLOG:  "SERVERLOG",
-    ID_CHANGE_REQUEST : "10",
+    NAME_CHANGE_REQUEST : "10",
     BAD_ID_FORMAT_REJECTED: "200",
     ALREADY_TAKEN_REJECTED: "201",
     CONFIRM_ID_CHANGE: "11",
-    USER_ID_CHANGE: "12",
+    NAME_CHANGE: "12",
+    CANNOT_SEND_DESCRIPTION: "100",
+    CANNOT_SEND_ICE_CANDIDATE: "101"
+};
+
+const extensions = {
+    SEND_OFFER: "send_offer",
+    RECEIVE_OFFER: "receive_offer",
     CONDITION_ACCEPTED: "A"
 };
+
+Object.assign(MESSAGES, extensions);
